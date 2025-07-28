a230efa7c1b2e3799b54a52f0f3f17f2f128e904
MOZHIVARMAN S <145837914+SMOZHIVARMAN@users.noreply.github.com>
2025-07-28 22:52:25 +0530

    #1

---
+++ Sorting/Bubble_sort.java
@@ -0,0 +1,32 @@
+//Repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order.
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    // Print array utility
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,32 @@
+//Finds the minimum (or maximum) element and puts it at the beginning (or end) of the array.
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int minIdx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            // Swap arr[minIdx] with arr[i]
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
